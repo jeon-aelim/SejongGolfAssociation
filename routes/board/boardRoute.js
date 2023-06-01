44c41fc52fcf8d3e570ee5cc9dce557d1df5dd53
jeon-aelim <devaelim@gmail.com>
2023-06-01 22:22:06 +0900

    first commit

--- routes/board/boardRoute.js
+++ routes/board/boardRoute.js
@@ -3,17 +3,20 @@ const router = express.Router();
 const boardController = require('./boardController');
 const upload = require('../../middleware/upload')
 
+// create
 router.post('/createContestPost', upload.uploadDocuments, boardController.createContestPost);
 router.post('/createAnnouncementPost' , upload.uploadDocuments,boardController.createAnnouncementPost);
 router.post('/createPhotoPost', upload.uploadImages, boardController.createPhotoPost);
 
+// delete
 router.get('/deletePost/:board_idx', boardController.deletePost);
 
+// edit
 router.post('/editContestPost', upload.uploadDocuments, boardController.editContestPost);
 router.post('/editAnnouncementPost', upload.uploadDocuments, boardController.editAnnouncementPost);
 router.post('/editPhotoPost',  upload.uploadImages, boardController.editPhotoPost);
 
-// token X
+// view (token X)
 router.get('/viewContestPost/:board_idx', boardController.viewContestPost); 
 router.get('/viewAnnouncementPost/:board_idx', boardController.viewAnnouncementPost); 
 router.get('/viewPhotoPost/:board_idx', boardController.viewPhotoPost); 
